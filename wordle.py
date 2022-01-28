2a6bd76258eb6fe998ee8dda137155cbc3faa95d
David <dyeung008@gmail.com>
2022-01-28 15:55:21 -0500

    Add keyboard and rework guesses printing

--- wordle.py
+++ wordle.py
@@ -4,43 +4,83 @@ import random
 ### Terminal printing Colors
 class bcolors:
     HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    OKGRAY = '\033[97m'
-    OKDGRAY = '\033[90m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
+    BLUE = '\033[94m'
+    CYAN = '\033[96m'
+    GREEN = '\033[92m'
+    GRAY = '\033[97m'
+    DGRAY = '\033[90m'
+    YELLOW = '\033[93m'
+    WHITE = '\033[37m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+### Function to print keyboard
+def keyboard():
+    for l in alphabet:
+        if l == "A":
+            print("\n", end= " ")
+        elif l == "Z":
+            print("\n", end= "  ")
+
+        if (status[l] == 0):
+            print(bcolors.WHITE + l ,end=" ")
+        elif (status[l] == 1):
+            print(bcolors.GREEN + l ,end=" ")
+        elif (status[l] == 2):
+            print(bcolors.YELLOW + l ,end=" ")
+        elif (status[l] == 3):
+            print(bcolors.DGRAY + l ,end=" ")
+    print()
+    print()
+
+
+### Function to print guess
+def print_guess():
+
+    for i in range(len(guess_book)):
+        chk = check_book[i]
+        gus = guess_book[i]
+
+        for j in range(5):
+            if (chk[j] == 0):
+                print(bcolors.WHITE + gus[j] ,end=" ")
+            elif (chk[j] == 1):
+                print(bcolors.GREEN + gus[j] ,end=" ")
+            elif (chk[j] == 2):
+                print(bcolors.YELLOW + gus[j] ,end=" ")
+            elif (chk[j] == 3):
+                print(bcolors.DGRAY + gus[j] ,end=" ")
+        print()
+    print()
+
 
 ### Load 5 letter words
-word_list = []
 f = open("5_letter_words.txt")
-for w in f:
-    word_list.append(w.rstrip().upper())
-
+word_list = [w.rstrip().upper() for w in f]
 
 ### pick a word
 answer = random.choice(word_list)
 
+### Create status check
+alphabet = "QWERTYUIOPASDFGHJKLZXCVBNM";
+status = dict(zip(alphabet, [0]*26))
+
 
 ### Guessing begins
 guess = ""
-book  = [ ];
 
-print(" ")
+guess_book  = [ ];
+check_book  = [ ];
+
 
 while (guess != answer):
 
     # ask for input
-    guess = input(bcolors.OKGRAY + "input your guess please! ").upper()
+    guess = input(bcolors.GRAY + "Input your guess please! ").upper()
 
     # tell the answer if asked
     if guess == "TELL ME":
-        print("ansewr is "+ answer)
+        print("The ansewr is " + bcolors.GREEN + answer)
         break
 
     # ask again if not the right length
@@ -52,21 +92,28 @@ while (guess != answer):
         continue
 
     # Check matching situations
-    check = ""
+    check = [0]*5
 
     for i in range(5):
         if guess[i] == answer[i]:
-            check = check + bcolors.OKGREEN + guess[i]
+            check[i] = 1
+            status[guess[i]] = 1
         else:
             if guess[i] in answer:
-                check = check + bcolors.WARNING + guess[i]
+                check[i] = 2
+                if status[guess[i]] != 1:
+                    status[guess[i]] = 2
             else:
-                check = check + bcolors.OKDGRAY + guess[i] 
+                check[i] = 3
+                status[guess[i]] = 3
 
     # append new guess
-    book.append(check)
+    check_book.append(check)
+    guess_book.append(guess)
             
     # print all guesses
-    for chk in book:
-        print(chk)
+    print_guess()
+
+    # Show keyboard
+    keyboard()
 
